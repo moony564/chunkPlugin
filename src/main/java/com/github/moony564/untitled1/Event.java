74fb9557bf4aecac8c1dcce575bdb025439d0f67
zoozk <zoozkid@naver.com>
2024-12-06 20:10:03 +0900

    [2024-12-06 20:10] seconde commit

--- src/main/java/com/github/moony564/untitled1/Event.java
+++ src/main/java/com/github/moony564/untitled1/Event.java
@@ -1,6 +1,27 @@
 package com.github.moony564.untitled1;
 
+import io.papermc.paper.event.packet.PlayerChunkLoadEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Event implements Listener {
+    @EventHandler
+    public void onChunkMove(PlayerMoveEvent e){
+        Chunk formChunk = e.getFrom().getChunk();
+        Chunk toChunk = e.getTo().getChunk();
+        Player p = e.getPlayer();
+
+        if(!formChunk.equals(toChunk)){
+            if (e.getPlayer().hasPermission("myplugin.bypass.chunkblock")) {
+                return; // 이동 허용
+            }
+            e.setCancelled(true);
+            p.damage(2.0f);
+            p.sendMessage(ChatColor.DARK_RED + "넘어갈수없음");
+        }
+    }
 }
--- src/main/java/com/github/moony564/untitled1/Untitled1.java
+++ src/main/java/com/github/moony564/untitled1/Untitled1.java
@@ -8,10 +8,12 @@ public final class Untitled1 extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new Event(), this);
+        getLogger().info("플러그인이 활성화 되었습니다.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("플러그인이 비활성화 되었습니다.");
     }
 }
